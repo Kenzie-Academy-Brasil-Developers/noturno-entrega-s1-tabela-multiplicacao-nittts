72500882634cd0a52b51ee6228d65f9f7da1518d
Nittts <w.baierle@gmail.com>
2021-10-18 20:56:29 -0300

    atividades realizadas

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,14 @@
-function nomeDaFuncao() { 
+function Multiplicacao(n) { 
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
     let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
-    }
+    for (let linha = 0; linha <= n; linha++) {
+        x.push([]);
+            for(let coluna = 0; coluna <= n; coluna++) {
+                x[linha][coluna] = linha * coluna
+            }
+    }   
     console.table(x)
 }
+
+Multiplicacao(12)
